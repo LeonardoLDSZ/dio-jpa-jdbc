f09d28facfe4469c2db464ec0cd653d32a4818a7
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-27 20:44:32 -0300

    first commit

---
+++ src/main/java/digitalinnovation/one/diojpajdbc/DioJpaJdbcApplication.java
@@ -0,0 +1,13 @@
+package digitalinnovation.one.diojpajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJpaJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJpaJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digitalinnovation/one/diojpajdbc/DioJpaJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalinnovation.one.diojpajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJpaJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
